d615ed16ccd8274d464ca7fa7ca8643dce74cc41
Cravatec <samuel.wrobel@gmail.com>
2022-06-13 16:33:52 +0200

    add data for PickerView

--- Miawouf/Controller/FormViewController.swift
+++ Miawouf/Controller/FormViewController.swift
@@ -9,21 +9,21 @@ import UIKit
 
 class FormViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+}
 
-        // Do any additional setup after loading the view.
+extension FormViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return dogRaces.count
     }
-    */
-
 }
+
+extension FormViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return dogRaces[row]
+    }
+}
+
